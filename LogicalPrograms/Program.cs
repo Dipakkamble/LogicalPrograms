7f159489d755fae131ce640705e250d6b2d3fb37
Dipakkamble <kambledipak700@gmail.com>
2023-02-02 16:52:32 +0530

    [Dipak] Modified fibonacci series

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -10,7 +10,17 @@ namespace LogicalPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to logical programs");
+            int a = 0, b = 1, c;
+            Console.WriteLine("Enter the number: ");
+            int num=Convert.ToInt32(Console.ReadLine());
+            Console.Write(a + " " + b + " ");
+            for (int i = 2; i < num; i++) 
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a=b; 
+                b=c;
+            }
             Console.ReadLine();
         }
     }
